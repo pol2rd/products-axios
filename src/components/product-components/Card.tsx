6b89a624f9b9e525e2e834432fefb085ff4f6721
LirPes <lironpes@@gmail.com>
2025-12-24 23:46:37 +0200

    pass card props and use it

--- src/components/product-components/Card.tsx
+++ src/components/product-components/Card.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Product } from '../../types';
+import { categories } from '../filters/DropDownFilter';
 
 interface CardProps {
+  product: Product
 }
 
-const Card: React.FC<CardProps> = () => {
+const Card: React.FC<CardProps> = (props: CardProps) => {
+  const maxRatings = 5
 
   return (
     <div className='product scale-effect'>
       <div className='product-image'>
-        <img />
+        <img src={props.product.image}/>
       </div>
       <div className='product-info'>
-        <h2 className='product-title'></h2>
+        <h2 className='product-title'>{props.product.title}</h2>
         <div className='product-brief'>
-          <p><strong>Price: </strong></p>
-          <p><strong>Rating: </strong></p>
-          <p><strong>Category: </strong></p>
+          <p><strong>{`Price: ${props.product.price}`}</strong></p>
+          <p><strong>{`Rating: ${props.product.rating.rate}/${maxRatings} (${props.product.rating.count} Reviews)`}</strong></p>
+          <p><strong>{`Category:  ${categories.find((category) => category.id == props.product.category)?.name}`}</strong></p>
         </div>
       </div>
     </div>
